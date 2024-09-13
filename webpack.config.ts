1b1bee03ed6e47f71292a5cf32a023550aaebc9a
Thomas Constantine Moore <tomismore@gmail.com>
2024-09-13 00:05:28 -0400

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  target: 'node',
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  mode: "development"
+};
+
+export default config;
